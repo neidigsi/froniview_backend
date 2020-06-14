4894fc96271c398b67ea6860a02356c55876160c
Simon Neidig <simon1234@online.de>
2020-06-14 15:44:10 +0200

    Initialize routes

---
+++ api/routes/route-month.js
@@ -0,0 +1,26 @@
+/*
+* All Request to the route "/category" will be forwarded in this module
+ */
+
+// Import node_modules
+const express = require('express')
+
+// Import own_modules
+
+// Initialize modules
+const router = express.Router()
+
+/*
+* Forward requests to the respective
+*
+* Allowed are only GET-Requests, because all categories will be defined by the App-Team itself and not
+* the users. To add a space to a category the user needs to edit the foreign key "categoryId" of the space
+* and not an entry of category.
+ */
+router.get('/', function (req, res, next) {
+    return res.status(200).json({
+        message: "Month route"
+    })
+})
+
+module.exports = router;
